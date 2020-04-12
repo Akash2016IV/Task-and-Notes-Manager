355348d0ffa022a3ef32f94d3c23cc148cb0a14e
Akash Bhardwaj <akash.bhardwaj@nagarro.com>
2020-04-12 23:59:08 +0530

    get task list from jquery on frontend

--- public/task_script.js
+++ public/task_script.js
@@ -15,14 +15,15 @@ window.onload = function(){
 }
 
 async function getAllTasks() {
+    let taskList = $('#taskList')
+    taskList.empty()
     const resp = await fetch('/tasks', { method: 'GET' })
-    ul.innerHTML = ''
-    const todos = await resp.json().then((jsonData) => {
-        jsonData.forEach(element => {
-          addTaskToPage(element)
-          })
-      })
-  }
+    await resp.json().then(taskData=>{
+        taskData.forEach(element => {
+            taskList.append(addTaskToPage(element))
+        })
+    }) 
+}
 
 function addTask(){
     let statusTask = false
@@ -49,25 +50,43 @@ async function addNewTaskJsonDB(title, due, description, status, priority) {
     })
   }
 
-  
+  function addTaskToPage(task){
+      let status = 'incomplete'
+      if(task.status === true){
+          status = 'complete'
+      }
+      return $(`      
+      <div class="row border">
+            <div class="col-sm border-right"> ${task.title}</div>
+            <div class="col-sm border-right">${task.description}</div>
+            <div class="col-sm border-right">${task.due}</div>
+            <div class="col-sm border-right">${status}</div>
+            <div class="col-sm border-right">${task.priority}</div>
+            <div class="col-sm">
+                <input class="btn-block" type="button" value="Update" id="update">
+            </div>
+        </div>
+        </div>`
+        )
+  }
 
-  function addTaskToPage(element){
-    let textNode = document.createTextNode(element.title)
-          li = document.createElement('li')
-          let checkbox = document.createElement('input')
-          checkbox.type='checkbox'
-          checkbox.setAttribute('id','check')
-          let label = document.createElement('label')
-          label.setAttribute('for','taskItem')
-          ul.appendChild(label)
-          li.appendChild(checkbox)
-          label.appendChild(textNode)
-          li.appendChild(label)
-          ul.insertBefore(li,ul.childNodes[0])
-        //   setTimeout(()=>{
-        //       li.className='visual'
-        //     },2)
-}
+//   function addTaskToPage(element){
+//     let textNode = document.createTextNode(element.title)
+//           li = document.createElement('li')
+//           let checkbox = document.createElement('input')
+//           checkbox.type='checkbox'
+//           checkbox.setAttribute('id','check')
+//           let label = document.createElement('label')
+//           label.setAttribute('for','taskItem')
+//           ul.appendChild(label)
+//           li.appendChild(checkbox)
+//           label.appendChild(textNode)
+//           li.appendChild(label)
+//           ul.insertBefore(li,ul.childNodes[0])
+//         //   setTimeout(()=>{
+//         //       li.className='visual'
+//         //     },2)
+// }
 
 function dueDateSetter(){
     const today = new Date()
